32f19993a1636fbdfd8d679db9de7929cceeae5c
Yerko Palma <yerko.palma@usach.cl>
2016-01-07 17:49:47 +0000

    first rails tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,45 +5,42 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  #test "should get index" do
+  #  get "/users"
+  #  assert_response :success
+  #  assert_not_nil assigns(:users)
+  #end
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { email: @user.email, name: @user.name }
+      post :create, { user: { email: @user.email, name: @user.name }, format: :json }
+      
+      assert_includes @response["Content-Type"], "application/json"
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    #expect(response.content_type).to eq("application/json")
   end
 
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
-  end
+  #test "should show user" do
+  #  get :show, id: @user
+  #  assert_response :success
+  #end
 
-  test "should get edit" do
-    get :edit, id: @user
-    assert_response :success
-  end
+  #test "should get edit" do
+  #  get :edit, id: @user
+  #  assert_response :success
+  #end
 
-  test "should update user" do
-    patch :update, id: @user, user: { email: @user.email, name: @user.name }
-    assert_redirected_to user_path(assigns(:user))
-  end
+  #test "should update user" do
+  #  patch :update, id: @user, user: { email: @user.email, name: @user.name }
+  #  assert_redirected_to user_path(assigns(:user))
+  #end
 
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, id: @user
-    end
+  #test "should destroy user" do
+  #  assert_difference('User.count', -1) do
+  #    delete "users/#{@user.id}.json"
+  #  end
 
-    assert_redirected_to users_path
-  end
+  #  assert_redirected_to users_path
+  #end
 end
