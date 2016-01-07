83e9f75b9448fd5e3ad71c073ab3afc68603523f
Yerko Palma <yerko.palma@usach.cl>
2016-01-07 14:03:54 +0000

    added delete action

--- app/assets/javascripts/users/userslist.js
+++ app/assets/javascripts/users/userslist.js
@@ -16,4 +16,33 @@ module.exports = Vue.extend({
                 console.log(JSON.stringify(response));
             });
         },
+        methods: {
+            erase: function(user){
+                if ( window.confirm('Are you sure?') ) {
+                    this.$http.delete(
+                        'users/' + user.id + '.json', 
+                        {}, 
+                        {headers: { //options
+                            'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+                            }
+                        }
+                    ).then(function(response){
+                        
+                        //force data reload
+                        this.$http.get('users.json').then(function(response){
+                
+                            this.users = response.data;
+                        }, function(response){
+                            
+                            console.log(JSON.stringify(response));
+                        });
+                        
+                        this.$route.router.go('/users');
+                    }, function(response){
+                        
+                        console.log(JSON.stringify(response));
+                    });
+                }
+            }
+        }
     });
